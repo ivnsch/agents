f66c26cd28cf3ef81d367313ca27302544759860
Ivan Schuetz <ivanschuetz@gmail.com>
2025-05-23 11:01:19 +0200

    Use manual view model instantiation
    `viewModel()` is not multi-platform

--- composeApp/src/commonMain/kotlin/com/schuetz/agents/App.kt
+++ composeApp/src/commonMain/kotlin/com/schuetz/agents/App.kt
@@ -5,12 +5,11 @@ import androidx.compose.runtime.*
 import com.schuetz.agents.chat.Chat
 import com.schuetz.agents.chat.ChatViewModel
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 @Preview
 fun App() {
-    val viewModel: ChatViewModel = viewModel()
+    val viewModel = remember { ChatViewModel() }
     MaterialTheme {
         Chat(viewModel)
     }
