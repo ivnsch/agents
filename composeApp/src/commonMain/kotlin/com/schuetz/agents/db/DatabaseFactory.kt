d9a386a20edf3e035c078fad71dcad2979b4f441
Ivan Schuetz <ivanschuetz@gmail.com>
2025-05-24 11:32:29 +0200

    Move DriverFactory to separate file

--- composeApp/src/commonMain/kotlin/com/schuetz/agents/db/DatabaseFactory.kt
+++ composeApp/src/commonMain/kotlin/com/schuetz/agents/db/DatabaseFactory.kt
@@ -1,11 +1,6 @@
 package com.schuetz.agents.db
 
-import app.cash.sqldelight.db.SqlDriver
 import com.schuetz.agents.Database
-
-expect class DriverFactory {
-    fun createDriver(): SqlDriver
-}
 
 interface DatabaseFactory {
     fun create(): Database
