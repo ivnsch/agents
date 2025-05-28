4f48f46327c70fe9dab4ea85cf4105ddcddc84e4
Ivan Schuetz <ivanschuetz@gmail.com>
2025-05-28 20:21:02 +0200

    Add missing dependency override to web

--- composeApp/src/wasmJsMain/kotlin/com/schuetz/agents/di/Modules.wasm.kt
+++ composeApp/src/wasmJsMain/kotlin/com/schuetz/agents/di/Modules.wasm.kt
@@ -4,10 +4,12 @@ import com.schuetz.agents.PrefsFactory
 import com.schuetz.agents.WebPrefsFactory
 import com.schuetz.agents.db.AgentsDao
 import com.schuetz.agents.db.MessagesDao
+import com.schuetz.agents.db.SpacesDao
 import com.schuetz.agents.db.WebDriverFactory
 import com.schuetz.agents.db.db.DriverFactory
 import com.schuetz.agents.db.mem.MemAgentsDao
 import com.schuetz.agents.db.mem.MemMessagesDao
+import com.schuetz.agents.db.mem.MemSpacesDao
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.js.JsClient
 import org.koin.core.module.Module
@@ -17,6 +19,7 @@ actual val platformModule: Module
     get() = module {
         single<MessagesDao> { MemMessagesDao() }
         single<AgentsDao> { MemAgentsDao() }
+        single<SpacesDao> { MemSpacesDao(get()) }
         single<DriverFactory> { WebDriverFactory() }
         single<HttpClientEngine> { JsClient().create() }
         single<PrefsFactory> { WebPrefsFactory() }
